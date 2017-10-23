21e92293ef83f1ba976041787aaaa9f4c7cceb20
n.aidar <n.aidar@gmail.com>
2017-10-23 19:07:19 +0300

    Добавьте файлы проекта.

---
+++ MatchingGame/Form1.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace MatchingGame
+{
+    public partial class Form1 : Form
+    {
+        Random random = new Random();
+        List<string> icons = new List<string>()
+        {
+            "!", "!", "N", "N", ",", ",", "k", "k",
+            "b", "b", "v", "v", "w", "w", "z", "z"
+        };
+        Label firstClicked = null;
+        Label secondClicked = null;
+        int timeLeft = 60;
+        int timeleftfirstclick = 3;
+        private SoundPlayer _soundPlayer;
+        private SoundPlayer _soundPlayer2;
+
+        public Form1()
+        {
+            InitializeComponent();
+            AssignIconsToSquares();
+            _soundPlayer = new SoundPlayer("true_match.wav");
+            _soundPlayer2 = new SoundPlayer("tada.wav");
+        }
+
+        private void AssignIconsToSquares()
+        {
+            // The TableLayoutPanel has 16 labels,
+            // and the icon list has 16 icons,
+            // so an icon is pulled at random from the list
+            // and added to each label
+            foreach (Control control in tableLayoutPanel1.Controls)
+            {
+                Label iconLabel = control as Label;
+                if (iconLabel != null)
+                {
+                    int randomNumber = random.Next(icons.Count);
+                    iconLabel.Text = icons[randomNumber];
+                    iconLabel.ForeColor = iconLabel.BackColor;
+                    icons.RemoveAt(randomNumber);
+                }
+            }
+        }
+
+        private void label_Click(object sender, EventArgs e)
+        {
+            if (!timer2.Enabled) timer2.Start();
+            // The timer is only on after two non-matching 
+            // icons have been shown to the player, 
+            // so ignore any clicks if the timer is running
+            if (timer1.Enabled == true)
+                return;
+
+            Label clickedLabel = sender as Label;
+
+            if (clickedLabel != null)
+            {
+                // If the clicked label is black, the player clicked
+                // an icon that's already been revealed --
+                // ignore the click
+                if (clickedLabel.ForeColor == Color.Yellow)
+                    return;
+
+                // If firstClicked is null, this is the first icon
+                // in the pair that the player clicked, 
+                // so set firstClicked to the label that the player 
+                // clicked, change its color to black, and return
+                if (firstClicked == null)
+                {
+                    timer3.Start();
+                    firstClicked = clickedLabel;
+                    firstClicked.ForeColor = Color.Yellow;
+                    return;
+                }
+
+                // If the player gets this far, the timer isn't
+                // running and firstClicked isn't null,
+                // so this must be the second icon the player clicked
+                // Set its color to black
+                timer3.Stop();
+                secondClicked = clickedLabel;
+                secondClicked.ForeColor = Color.Yellow;
+
+                // Check to see if the player won
+                CheckForWinner();
+
+                // If the player gets this far, the player 
+                // clicked two different icons, so start the 
+                // timer (which will wait three quarters of 
+                // a second, and then hide the icons)
+
+                // If the player clicked two matching icons, keep them 
+                // black and reset firstClicked and secondClicked 
+                // so the player can click another icon
+                if (firstClicked.Text == secondClicked.Text)
+                {
+                    firstClicked = null;
+                    secondClicked = null;
+                    timer3.Stop();
+                    _soundPlayer.Play();
+                    return;
+                }
+                timer1.Start();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // Stop the timer
+            timer1.Stop();
+
+            // Hide both icons
+            firstClicked.ForeColor = firstClicked.BackColor;
+            secondClicked.ForeColor = secondClicked.BackColor;
+
+            // Reset firstClicked and secondClicked 
+            // so the next time a label is
+            // clicked, the program knows it's the first click
+            firstClicked = null;
+            secondClicked = null;
+        }
+
+        private void CheckForWinner()
+        {
+            // Go through all of the labels in the TableLayoutPanel, 
+            // checking each one to see if its icon is matched
+            foreach (Control control in tableLayoutPanel1.Controls)
+            {
+                Label iconLabel = control as Label;
+
+                if (iconLabel != null)
+                {
+                    if (iconLabel.ForeColor == iconLabel.BackColor)
+                    {
+                        return;
+                    }
+                        
+                }
+            }
+
+            // If the loop didn’t return, it didn't find
+            // any unmatched icons
+            // That means the user won. Show a message and close the form
+            timer2.Stop();
+            _soundPlayer2.Play();
+            MessageBox.Show("You matched all the icons!", "Congratulations");
+            
+            
+            Close();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if (timeLeft > 0)
+            {
+                // Display the new time left
+                // by updating the Time Left label.
+                timeLeft--;
+                timerLabel.Text = timeLeft + " seconds";
+            }
+            else
+            {
+                // If the user ran out of time, stop the timer, show
+                // a MessageBox, and fill in the answers.
+                timer2.Stop();
+                timerLabel.Text = "Time's up!";
+                MessageBox.Show("You didn't finish in time.", "Sorry!");
+                Close();
+            }
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            if (timeleftfirstclick > 0)
+            {
+                // Display the new time left
+                // by updating the Time Left label.
+                timeleftfirstclick--;
+
+            }
+            else
+            {
+                // If the user ran out of time, stop the timer, show
+                // a MessageBox, and fill in the answers.
+                firstClicked.ForeColor = firstClicked.BackColor;
+                firstClicked = null;
+                timer3.Stop();
+            }
+        }
+    }
+}
